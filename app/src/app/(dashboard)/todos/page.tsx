f0285d3810b185ad8ce9f13293c688dde52d89cc
joncoe <info@joncoe.com>
2024-01-30 18:29:50 +0900

    Add ToDo and ToDoList components Update alias in tsconfig

--- app/src/app/(dashboard)/todos/page.tsx
+++ app/src/app/(dashboard)/todos/page.tsx
@@ -1,5 +1,19 @@
-const ToDosPage = () => {
-  return <div>ToDosPage</div>
+import TodoList from '@/app/components/ToDoList'
+import db from '@/app/utils/db'
+
+const getData = async () => {
+  const todos = await db.todo.findMany({})
+  return todos
+}
+
+const ToDosPage = async () => {
+  const todos = await getData()
+  return (
+    <>
+      <h1>To Do List</h1>
+      <TodoList todos={todos} />
+    </>
+  )
 }
 
 export default ToDosPage
---
+++ app/src/app/components/ToDoList.tsx
@@ -0,0 +1,13 @@
+import Todo from './ToDo'
+
+const TodoList = ({ todos }) => {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo.id} />
+      ))}
+    </div>
+  )
+}
+
+export default TodoList
